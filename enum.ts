4a3f9dcef33c1672d8644089bef34356648c51db
gufran0 <md.gufran@techchefz.com>
2024-11-25 14:53:57 +0530

    enums

---
+++ enum.ts
@@ -0,0 +1,12 @@
+// enum is a way to define a set of named constants
+
+const enum SeatChoice {
+  AISLE = "aisle",
+  MIDDLE = 3,
+  WINDOW,
+  FOURTH,
+}
+
+const hcSeat = SeatChoice.AISLE;
+
+export {};
---
+++ tuple.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tuser = ["hc", 123, true];
+// in tuple we have fixed number of elements and fixed types
+// if we define a tuple with 3 elements then we cannot add more than 3 elements to it
+// tuple is very helpful in api calls databases and other places where we want to store a specific type of data
+var rgb = [255, 123, 112];
+var newUsere = [112, "example@example.com"];
+newUsere[1] = "hc.com";
